56784f1817eb4fa35e63d384388f2612f14c9d9c
Sam Schmitt <sam@samschmitt.com>
2021-07-01 09:45:32 -0400

    removed codepen from query

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -19,7 +19,6 @@ const useSiteMetadata = () => {
                 email
                 rss
                 gitlab
-                codepen
                 youtube
                 soundcloud
                 microblog
