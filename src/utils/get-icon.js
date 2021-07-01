5f3ee2772ea798674932e0b404b79035996216c6
Sam Schmitt <sam@samschmitt.com>
2021-07-01 10:02:01 -0400

    Added SVG micro.blog icon

--- src/utils/get-icon.js
+++ src/utils/get-icon.js
@@ -50,6 +50,9 @@ const getIcon = (name: string) => {
     case 'soundcloud':
       icon = ICONS.SOUNDCLOUD;
       break;
+    case 'microblog':
+      icon = ICONS.MICROBLOG;
+      break;
     default:
       icon = {};
       break;
